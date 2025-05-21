c0ef655c0213eea90921be85b274c5290742c4fb
Sergei Uss <sashikhmin@ewu.edu>
2025-05-21 14:27:31 -0700

    new tags and layers with arrow fly logic

---
+++ Assets/Scripts/Attack/Attack.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+
+    public GameObject arrowPrefab;
+    public Transform firePoint;
+
+    [Header("Charging Settings")] 
+    public float shootForce = 10f;
+    public float shootCooldown = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Aim toward mouse
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        // Shoot on click with cooldown
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject arrow = Instantiate(arrowPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.right * shootForce, ForceMode2D.Impulse);
+    }
+}
