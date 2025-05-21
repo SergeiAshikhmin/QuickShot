b993f7d5614a6c55e4d9aab24412cdedbf4001e1
Sergei Uss <sashikhmin@ewu.edu>
2025-05-21 12:28:55 -0700

    Arrow init

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public GameObject arrowPrefab;
+    public Transform firePoint;
+
+    [Header("Charging Settings")] 
+    public float minForce = 5f;
+    public float maxForce = 10f;
+    public float chargeSpeed = 10f;
+
+    private float currentForce;
+    private bool isCharging = false;
 
     // Update is called once per frame
     void Update()
@@ -23,5 +28,30 @@ public class Attack : MonoBehaviour
         // Rotate the bow to face the mouse
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        if (Input.GetMouseButton(0))
+        {
+            isCharging = true;
+            currentForce = minForce;
+        }
+        
+        if (Input.GetMouseButton(0) && isCharging) // While holding
+        {
+            currentForce += chargeSpeed * Time.deltaTime;
+            currentForce = Mathf.Clamp(currentForce, minForce, maxForce);
+        }
+
+        if (Input.GetMouseButtonUp(0) && isCharging) // Release to shoot
+        {
+            Shoot(currentForce);
+            isCharging = false;
+        }
+    }
+
+    void Shoot(float force)
+    {
+        GameObject arrow = Instantiate(arrowPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.right * force, ForceMode2D.Impulse);
     }
 }
