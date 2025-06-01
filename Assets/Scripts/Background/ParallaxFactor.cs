d550c9abbebd5b020cda83b99e545ecce23a17e2
Ethan Guerin <129787373+EthanAGi@users.noreply.github.com>
2025-05-31 20:15:19 -0700

    Revert "Merge remote-tracking branch 'origin/Level1' into dev"
    This reverts commit ec02116041e60d91ac8c233e69d4100456298dc3, reversing
    changes made to a7b24268a1d5aa75c0c41aa408dada066adb1fa0.

--- Assets/Scripts/Background/ParallaxFactor.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[DisallowMultipleComponent]
-public class ParallaxFactor : MonoBehaviour
-{
-    [Range(0f, 1f)] public float factor = 0.3f;
-}
